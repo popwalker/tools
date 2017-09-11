fb45d09f7b30aaf00b33e520ecf9c75f3959b507
changxf <kevintwenty1@gmail.com>
2017-09-11 21:56:11 +0800

    add a concurrency control demo code

---
+++ concurrency/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+	"github.com/jeffail/tunny"
+	"time"
+	"math/rand"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/runWorker", http.HandlerFunc(myhandle))
+	http.ListenAndServe(":1234", nil)
+}
+
+func myhandle(w http.ResponseWriter, r *http.Request) {
+	go consume()
+	w.Write([]byte("running some worker"))
+}
+
+func consume() {
+	var urls []string
+	// make some url list
+	for i := 1; i <= 100; i++ {
+		tmp := "url_" + strconv.Itoa(i) + ".html"
+		urls = append(urls, tmp)
+	}
+
+	// create a pool with a handle func: send
+	pool, _ := tunny.CreatePool(10, send).Open()
+
+	//defer pool.Close()
+
+	// circulate url list and async send to worker,handle response with func: callback
+	for _, url := range urls {
+		pool.SendWorkAsync(url, callback)
+	}
+}
+
+func send(object interface{}) interface{} {
+	input := object.(string)
+	sleepNumber := rand.Intn(5)
+	fmt.Println(time.Now().String() + "got data " + input + ", and will sleep " + strconv.Itoa(sleepNumber) + " second")
+	time.Sleep(time.Duration(sleepNumber) * time.Second)
+	output := input
+	return output
+}
+
+func callback(resp interface{}, err error) {
+	res := resp.(string)
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Println("got response data:" + res)
+}
